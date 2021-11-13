f11b70ad97a1cf93866ca34110164b1f5c3940fb
Joao-Gontijo <jvgontijo166@gmail.com>
2021-11-13 14:06:43 -0300

    JSP listaEmpresa

--- src/main/java/br/com/alura/gerenciador/servlets/ListaEmpresasServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlets/ListaEmpresasServlet.java
@@ -1,9 +1,9 @@
 package br.com.alura.gerenciador.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -24,16 +24,11 @@ public class ListaEmpresasServlet extends HttpServlet {
 		Banco banco = new Banco();
 		List<Empresa> lista = banco.getEmpresas();
 		
-		PrintWriter out = response.getWriter();
-		out.println("<html><body>");
-		out.println("<ul>");
+		request.setAttribute("empresas", lista);
+		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresaCriada.jsp");
+		rd.forward(request, response);
 		
-		for (Empresa empresa : lista) {
-			out.println("<li>" + empresa.getNome() + "</li>");
-		}
-		
-		out.println("</ul>");
-		out.println("</body></html>");
+
 	}
 
 }
