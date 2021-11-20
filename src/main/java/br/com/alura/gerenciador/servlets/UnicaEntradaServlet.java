81afbd9928a50708f541c001690fbb348d6ab48e
Joao-Gontijo <jvgontijo166@gmail.com>
2021-11-19 22:53:24 -0300

    autorização de acesso

--- src/main/java/br/com/alura/gerenciador/servlets/UnicaEntradaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlets/UnicaEntradaServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import br.com.alura.gerenciador.acoes.Acao;
 import br.com.alura.gerenciador.acoes.AlteraEmpresa;
@@ -28,11 +29,21 @@ public class UnicaEntradaServlet extends HttpServlet {
 	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		String paramAcao = request.getParameter("acao");
-		 
+		
+		HttpSession sessao = request.getSession();
+		boolean usuarioNaoLogado = (sessao.getAttribute("usuarioLogado") == null);
+		boolean ehUmaAcaoProtegida = !(paramAcao.equals("Login") || paramAcao.equals("LoginForm"));
+		
+		if(ehUmaAcaoProtegida && usuarioNaoLogado) {
+			 response.sendRedirect("entrada?acao=LoginForm");
+			 return;
+		}
+		
+		
 		
 		String nomeClasse = "br.com.alura.gerenciador.acoes." + paramAcao;
-		
 		String nome;
 		try {
 			@SuppressWarnings("rawtypes")
